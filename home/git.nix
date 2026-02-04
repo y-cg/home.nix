8f91b674d451fae6d180a408246bf4143f65e191
y-cg <159802291+y-cg@users.noreply.github.com>
2026-02-04 19:39:41 +0800

    feat(git): configure git configs

--- home/git.nix
+++ home/git.nix
@@ -1,6 +1,38 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  home.packages = with pkgs; [
-    git
-  ];
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "y-cg";
+        email = "159802291+y-cg@users.noreply.github.com";
+      };
+
+      init.defaultBranch = "main";
+
+      alias = {
+        st = "status";
+        co = "checkout";
+        br = "branch";
+        ci = "commit";
+        dl = "log -p --ext-diff";
+      };
+
+      push = {
+        autoSetupRemote = true;
+      };
+
+      # https://git-scm.com/book/en/v2/Git-Tools-Rerere
+      rerere = {
+        enabled = true;
+      };
+
+      # let git handle the whitespace in a proper way
+      apply = {
+        ignoreWhitespace = "change";
+        whitespace = "fix";
+      };
+    };
+  };
+
 }
